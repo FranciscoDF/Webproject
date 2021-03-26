49dbe26869dbc967182a678e361de5ad19d1faa9
Francisco <f.de.faria@free.fr>
2021-03-26 10:38:40 +0100

    ok

---
+++ Controller/connexion.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="a propos.css">
+        <title>Connexion à votre compte CESI'NDEED</title>
+    </head>
+
+    <body>
+        <main>
+                    
+            <form method= "POST" action="page_connexion.php">
+                <section style="text-align: center;">
+                    <h2> Connexion:</h2>
+                    <fieldset style=" padding: 6px; margin-bottom: 40%; margin-left: 35%; margin-right: 35%; ">
+                        <h5>Identitiant :</h5>
+                        <input id='id' type="text" name="id" required />
+                        <h5>Mots de passe :</h5>
+                        <input id='mdp' type="password" name="mdp" required/>
+                        <br>
+                        <br>
+                        <input id="garder" type="checkbox" name="garder"><label for="garder"> rester connecté</label>
+                        <br>
+                        <br>
+                        <input id='submit' type="submit" value="Valider">
+                    </fieldset>
+                    <?php
+                    /*if(isset($_GET['erreur'])){
+                        $err = $_GET['erreur'];
+                        if($err==1)
+                            echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                    }*/
+                    if(isset($_GET['erreur'])) {
+                        $err = $_GET['erreur'];
+                        if($err==1)
+                            echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                    }
+                    ?>
+                </section>
+            </form>
+
+        </main>
+    </body>
+</html>
