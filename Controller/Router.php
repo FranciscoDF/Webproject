9d59828229ff9d34ab9943485434abade9120908
Francisco <f.de.faria@free.fr>
2021-03-26 13:50:39 +0100

    router

---
+++ Controller/Router.php
@@ -0,0 +1,19 @@
+<?php 
+class Router
+{
+    private $_ctrl;
+    private $_view;
+
+    public function routeReq() {
+        try {
+            // Chargement des classes automatiquement
+            spl_autoload_register(function($class){
+                require_once('models/'.$class.'.php')
+            });
+
+            $url = '';
+        } catch (Exceptiob $e){
+
+        }
+    }
+}
