13c2cc1666445db822e4ddd2e3ced9462f868365
emilePanchout <ping0yn76@gmail.com>
2021-03-26 14:24:21 +0100

    commit

--- Model/user.php
+++ Model/user.php
@@ -11,13 +11,28 @@ class user {
     public $center_id;
    
    
-    function getId() {
-    
-    }
+function get() {8 attributs }
+
+    function getId() {}
+    function getUsername() {}
+    function getPassword() {}
+    function getFname() {}
+    function getLname() {}
+    function getRoleID() {}
+    function getPromoID() {}
+    function getCenterID() {}
+
+    function modifyUsername() {}
+    function modifyPassword() {}
+    function modifyFname() {}
+    function modifyLname() {}
+    function modifyRoleID() {}
+    function modifyPromoID() {}
+    function modifyCenterID() {}
 }
 
 function getUsers(){
-
+/*
     include 'Connecion_BDD.php';
 
     $pdoQuery = "SELECT * FROM `utilisateur` ";
@@ -28,18 +43,9 @@ function getUsers(){
     $verify = $pdo->query($pdoQuery);
     $fetch = $verify->fetch(PDO::FETCH_ASSOC);
     print_r($fetch);
-
-
+*/
 
 }
-
-
-
-
-
-
-
-
 
 
 
