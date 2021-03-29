d79e1d531d7979bcff9103dac6b430c22e88a2c8
hugo-per <73825403+hugo-per@users.noreply.github.com>
2021-03-29 11:06:32 +0200

    view

---
+++ View/ViewUser.php
@@ -0,0 +1,15 @@
+<body>
+<div class="titre">
+      <h1>
+        USERS
+      </h1>
+    </div>
+    <div class="choose">
+      <select name="sort" id="sort-by">
+        <option value="">Sort by</option>
+        <option value="alph">A-Z</option>
+        <option value="-alph">Z-A</option>
+        <option value="age">Age</option>
+    </select>   
+    </div>
+</body>
