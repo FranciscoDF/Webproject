0d7c1f22e5fdde1bd2ccfdb90216f565848a4eb6
hugo-per <73825403+hugo-per@users.noreply.github.com>
2021-03-29 15:07:51 +0200

    accueil / user

---
+++ View/accueil.php
@@ -0,0 +1,57 @@
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="accueil.css">
+    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
+</head>
+
+<body>
+    <main>
+        <header>
+            <div class="logo">
+                <img src="cesi.png"><br><br><br><br>
+            </div>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">Menu</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="accueil.php">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="entreprises.php">Industries</a>
+                    <a class="nav-item nav-link" href="utilisateurs.php">Users</a>
+                    <a class="nav-item nav-link" href="stages.php">Internship</a>
+                    <a class="nav-item nav-link" href="account.php">Account</a>
+                  </div>
+                </div>
+              </nav>
+        </header>
+    
+        <div class="titre">
+            <h1>
+                CESI'NDEED BEST PLACE TO FIND AN INTERNSHIP
+            </h1>
+        </div>
+        <div class="button_a">
+                <button id="go">Search internship</button>
+        </div>
+    </main>
+    <footer>
+        <p>
+            Mentions légales <br>
+        <p>
+            <p>
+                woula
+            </p>
+            <script src="./assets/vendors/jquery/jquery-3.5.1.min.js"></script>
+    </footer>
+    <script src="./assets/vendors/jquery/jquery-3.5.1.min.js"></script>
+    <script src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
