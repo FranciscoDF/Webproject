ba2b0e320713f9a7d07d01418f73516a5d80df67
Francisco <f.de.faria@free.fr>
2021-03-29 14:40:37 +0200

    test

--- Controller/ControllerUser.php
+++ Controller/ControllerUser.php
@@ -7,18 +7,23 @@ class ControllerUser
     private $_view; 
 
     public function __construct($url) {
+        echo 'il faut appeler la fonction : '.$url[1];
         if (isset($url) && count($url) > 1){
             throw new Exception('Page introuvable');
-        } else
-            $this->user();
+        }
     }
 
-    private function user() {
+    private function userview() {
         $this->_userManager = new userManager;
         $user= $this->_userManager->getList();
 
         $this->_view = new View('User');
         $this->_view->generate(array('user' => $userView));
+    }
+
+    private function useradd(user $user) {
+        $this->_userManager = new userManager;
+        $user= $this->_userManager
     }
 
 }
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once('Controller/router.php');
 $router = new Router();
 $router-> routeReq();
 
