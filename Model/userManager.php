7421290e9575b8d221116ce19aef4df639544a72
Jean GONCALVES <jean.goncalves@viacesi.fr>
2021-03-29 12:07:02 +0200

    Update userManager.php

--- Model/userManager.php
+++ Model/userManager.php
@@ -34,12 +34,12 @@ class userManager {
     public function add(user $user) {
         $query = $this->_co->prepare('INSERT INTO utilisateur(user_utilisateur, mdp_utilisateur, nom_utilisateur, prenom_utilisateur, id_role, id_promotion, id_centre) VALUES (:user_utilisateur, :mdp_utilisateur, :nom_utilisateur, :prenom_utilisateur, :id_role, :id_promotion, :id_centre)');
         $query->execute([
-            'user_utilisateur'=> $user->username_user(),
-            'mdp_utilisateur'=> $user->password_user(),
-            'mdp_utilisateur'=> $user->fname_user(),
-            'mdp_utilisateur'=> $user->lname_user(),
-            'mdp_utilisateur'=> $user->id_role(),
-            'mdp_utilisateur'=> $user->id_promo(),
+            'user_utilisateur'=> $user->username_user();
+            'mdp_utilisateur'=> $user->password_user();
+            'mdp_utilisateur'=> $user->fname_user();
+            'mdp_utilisateur'=> $user->lname_user();
+            'mdp_utilisateur'=> $user->id_role();
+            'mdp_utilisateur'=> $user->id_promo();
             'mdp_utilisateur'=> $user->id_center()
         ]);
     }
