cb04a9843db3444efd2eb61c7cf999f8e01394cb
Jean GONCALVES <jean.goncalves@viacesi.fr>
2021-03-30 14:39:21 +0200

    view fonctionnelle industries

--- View/ViewIndustries.php
+++ View/ViewIndustries.php
@@ -1,10 +1,15 @@
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8"> 
+    <link rel="stylesheet" href="View/entreprises.css">
+    <link rel="stylesheet" href="View/template.css">
+</head>
+
 <body>
-<div class="titre">
-      <h1>
-        Industries
-      </h1>
-    </div>
-    <div class="choose">
+<div class="choose">
       <select name="sort" id="sort-by">
         <option value=""><p>Sort by</p></option>
         <option value="alph">A-Z</option>
@@ -12,6 +17,31 @@
         <option value="age">Age</option>
     </select>   
     </div>
+<div>
+      
+    <table id='empTable' class='dataTable'><tr>
+      <tr>
+      <th><h2>Compagny Name</h2></th>
+      <th><h2>Compagny Location</h2></th>
+      <th><h2>Evaluation</h2></th>
+      <th><h2>Trainee Number</h2></th>
+      <th><h2>Description</h2></th>
+      <th><h2>Id Secteur</h2></th>
+      </tr>
+
+      <?php $this->_t= 'Industries';
+      foreach($industries as $industries): ?>
+      <tr><td><h2><?= $industries->name_entreprise() ?></h2></td>
+      <td><h2><?= $industries->lieu_entreprise() ?></h2></td>
+      <td><h2><?= $industries->evalMoy_entreprise() ?></h2></td>
+      <td><h2><?= $industries->nbStage_entreprise() ?></h2></td>
+      <td><h2><?php $test=$industries->des_entreprise(); switch ($test) {case '1' : echo 'Etudiant';} ?></h2></td>
+      <td><h2><?= $industries->id_secteur() ?></h2></td>
+      <?php endforeach; ?>
+      </tr></table>
+    </div>
+  
+    
     <div class="button_a">
   <button id="go">Add</button>
   <button id="go">Update</button>
