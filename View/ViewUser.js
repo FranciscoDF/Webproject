a375d3c4aa9fbc192e3769df6a6ea965f5221934
emilePanchout <ping0yn76@gmail.com>
2021-03-30 23:26:16 +0200

    JS des boutons CRUD (il faut mettre les bons url)

--- View/ViewUser.js
+++ View/ViewUser.js
@@ -1,3 +1,17 @@
 document.getElementById('adder').onclick = function() {
     
+    document.location.assign("url de la page add");
+//////////////////Rajouter l'url de la page add dans la fonction assign
+}
+
+document.getElementById('updater').onclick = function() {
+    
+    document.location.assign("url de la page update");
+//////////////////Rajouter l'url de la page update dans la fonction assign
+}
+
+document.getElementById('url de la page delete').onclick = function() {
+    
+    document.location.assign("");
+//////////////////Rajouter l'url de la page delete dans la fonction assign
 }
