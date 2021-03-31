85f049f27abe483a1ac45688585df331605c2191
Jean GONCALVES <jean.goncalves@viacesi.fr>
2021-03-31 12:09:09 +0200

    modif 3

--- View/ViewUserAdd.php
+++ View/ViewUserAdd.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="Companies.css"/>
+        
+       
     </head>
     <body class="corps">
         <header>
