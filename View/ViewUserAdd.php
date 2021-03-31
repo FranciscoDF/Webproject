1bc5bda42db607c3bb58d656075834e527b1611e
Francisco <f.de.faria@free.fr>
2021-03-31 12:15:32 +0200

    ui

--- View/ViewUserAdd.php
+++ View/ViewUserAdd.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="Companies.css"/>
+        <link rel="stylesheet" type="text/css" href="View/companies.css"/>
     </head>
     <body class="corps">
         <header>
