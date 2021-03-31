ac703ad1766a1593ad4d00e96aff25e267d015fc
Francisco <f.de.faria@free.fr>
2021-03-31 23:32:13 +0200

    modif

--- View/template.php
+++ View/template.php
@@ -15,7 +15,7 @@
     <main>
     <header>
       <div class="logo">
-        <a href="http://localhost/ProjetWeb/Webproject/?url=home"><img src="View/cesi.png"></a><br><br><br><br>
+        <a href="./?url=home"><img src="View/cesi.png"></a><br><br><br><br>
       </div>
       
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -28,13 +28,13 @@
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item active">
-              <a class="nav-link" href="http://localhost/ProjetWeb/Webproject/?url=home">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="./?url=home">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="http://localhost/ProjetWeb/Webproject/?url=industries/view">Industries</a>
+              <a class="nav-link" href="./?url=industries/view">Industries</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="http://localhost/ProjetWeb/Webproject/?url=user/view">Users</a>
+              <a class="nav-link" href="./?url=user/view">Users</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="stages.php">Internship</a>
