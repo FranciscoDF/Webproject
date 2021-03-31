4017eb68aa2c8f76a04028338a0b1af983532683
hugo-per <73825403+hugo-per@users.noreply.github.com>
2021-03-31 14:33:06 +0200

    view delete internship, industries

---
+++ View/ViewIndustriesdelete.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="View/companies.css"/>
+    </head>
+    <body class="corps">
+        <header>
+        </header>
+            <form action="http://localhost/ProjetWeb/Webproject/?url=industries/delete" method="post" class="formul">
+                <section>
+                    <fieldset class="formb1">
+                    <label> ID Industries</label>
+                        <input type="text" class="textbox" id="ID" name="user"/>
+                    </fieldset>
+                        <div class="button_a">
+                            <button id="go_d">Delete</button>
+                        </div>
+                    </fieldset>
+                </section>
+            </form>
+    </body>
+</html>
